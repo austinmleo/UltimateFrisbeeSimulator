df1299a0fe87c47afac747780397aa3e244723ef
chasetyree <ttyree@mymail.mines.edu>
2013-11-14 12:33:50 -0700

    Added NumPlayers and Five Each team tests

--- src/Tests/JUnitTests.java
+++ src/Tests/JUnitTests.java
@@ -2,13 +2,41 @@ package Tests;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class JUnitTests {
+import Game.Game;
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+public class JUnitTests {
+	private static Game game;
+	
+	@BeforeClass
+	public void setUp(){
+		game = new Game();
 	}
 
+	@Test
+	public void numberOfPlayers() {
+		int players = game.HumanPlayer.size() + game.ComputerPlayer.size();
+		assertEquals(players,10);
+	}
+
+	@Test
+	public void FiveOnEachTeam() {
+		assertEquals(game.HumanPlayer.size(), 5);
+		assertEquals(game.ComputerPlayer.size(), 5);
+	}
+	
+	@Test
+	public void PlayerHasFrisbee() {
+		
+	}
+	
+	@Test
+	public void AllPlayersInDifferentPlaces() {
+		
+	}
+	
+	
 }
